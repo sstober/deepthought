dfc2520490ecb4cf8f860119a5d8302aca12acc7
Sebastian Stober <sstober@uwo.ca>
2015-06-04 18:19:31 -0400

    extended to multiple classes

--- deepthought/datasets/eeg/meta_class.py
+++ deepthought/datasets/eeg/meta_class.py
@@ -2,27 +2,31 @@ __author__ = 'sstober'
 
 from deepthought.datasets.eeg.EEGEpochsDataset import DataFile
 
-class DataFileWithMetaClass(DataFile):
-    def __init__(self, filepath, class_name, classes):
-        super(DataFileWithMetaClass, self).__init__(filepath)
+class DataFileWithMetaClasses(DataFile):
+    def __init__(self, filepath, meta_classes=dict()):
+        super(DataFileWithMetaClasses, self).__init__(filepath)
 
-        # create meta class
-        class_map = dict()
-        max_value = 0
-        for meta in self.metadata:
-            key = []
-            for label in classes:
-                key.append(meta[label])
-            key = tuple(key)
+        for class_name, classes in meta_classes.iteritems():
+            self._create_meta_class(class_name, classes)
 
-            if not key in class_map:
-                class_map[key] = max_value
-                max_value += 1
 
-            value = class_map[key]
+    def _create_meta_class(self, class_name, classes):
+            class_map = dict()
+            max_value = 0
+            for meta in self.metadata:
+                key = []
+                for label in classes:
+                    key.append(meta[label])
+                key = tuple(key)
 
-            meta[class_name] = value
-            # print key, value, meta
+                if not key in class_map:
+                    class_map[key] = max_value
+                    max_value += 1
+
+                value = class_map[key]
+
+                meta[class_name] = value
+                # print key, value, meta
 
 
 # target_processor implementation
