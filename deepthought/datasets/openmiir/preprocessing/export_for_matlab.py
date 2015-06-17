4c29820722cb549a63fc30e1261d97bbe2034380
Sebastian Stober <sstober@uwo.ca>
2015-06-16 22:47:37 -0400

    added events export for matlab

--- deepthought/datasets/openmiir/preprocessing/export_for_matlab.py
+++ deepthought/datasets/openmiir/preprocessing/export_for_matlab.py
@@ -1,10 +1,17 @@
 __author__ = 'sstober'
 
-import os
+import numpy as np
 from scipy import io
 
 def export_events_to_matlab(events, output_filepath):
-    pass # TODO
+    # EEGLab event structure: type, latency, urevent
+    # Event latencies are stored in units of data sample points relative to (0)
+    # the beginning of the continuous data matrix (EEG.data).
+    eeglab_events = [[event[2], event[0], 0] for event in events]
+    eeglab_events = np.asarray(eeglab_events, dtype=int)
+
+    # print 'saving events to', output_filepath
+    io.savemat(output_filepath, dict(data=eeglab_events), oned_as='row')
 
 def export_raw_to_matlab(raw, output_filepath):
 
